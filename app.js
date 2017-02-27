8bc61c230a02096981f0a2aa3bd0741ea5dc4cf9
Jordan Skomer <jordan.skomer@gatewaystaff.com>
2017-02-26 22:18:31 -0600

    Use Redis for RSVPS

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var app = express();
 var path = require('path');
 const fs = require('fs');
 var bodyParser = require("body-parser");
-
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = require("redis").createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  var client = require("redis").createClient();
+}
 
 // views as directory for all template files
 app.set('views', path.join(__dirname, 'views'));
@@ -19,21 +25,25 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/super-secret-path', function(req, res) {
+  client.lrange("rsvps", 0, -1, function(err, rsvps){
+    console.log(rsvps);
+    var jsonRsvps = [];
+    for (var i = 0; i < rsvps.length; i++) {
+      jsonRsvps.push(JSON.parse(rsvps[i]));
+    }
+    res.send(jsonRsvps);
+  });
+});
+
 app.post('/', function(req, res) {
-  fs.readFile('rsvps.json', function (err, data) {
-    var json = JSON.parse(data);
-    json.push(req.body);
-    console.log(json);
-    fs.writeFile("rsvps.json", JSON.stringify(json), function(err) {
-      if(err) {
-          return console.log(err);
-      }
-      console.log("--------------------------")
-      console.log("RSVP Submitted")
-      console.log("--------------------------")
-      console.log(req.body)
-      console.log("--------------------------")
-    });
+  client.lpush("rsvps", JSON.stringify(req.body), function(err, reply){
+    console.log(reply);
+    console.log("--------------------------")
+    console.log("RSVP Submitted")
+    console.log("--------------------------")
+    console.log(req.body)
+    console.log("--------------------------")
   });
 });
 // Set server port
